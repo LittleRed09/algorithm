050b291a16a32f9538afe13372e372f72c21e2e4
lucawu <lucawu@tencent.com>
2021-08-26 19:37:54 +0800

    offer 13 DFS version-2

--- movingCount-DFS.js
+++ movingCount-DFS.js
@@ -5,24 +5,20 @@
  * @param {number} k
  * @return {number}
  */
-var movingCount = function(m, n, k) {
+ var movingCount = function(m, n, k) {
     let set = new Set();
-    let result = 0;
     dfs(0,0)
+    return set.size;
     
     function dfs(i, j){
-        if(i < 0 || j < 0 || i > m - 1 || j > n - 1|| forbid(i,j,k)){
+        if(i > m - 1 || j > n - 1 || forbid(i,j,k)){
             return;
         }
         let spot = i+','+j
         if(!set.has(spot)){
             set.add(spot)
-            result++
             dfs(i+1,j)
             dfs(i,j+1)
-            // 只需要向下和向右遍历
-            // dfs(i,j-1)
-            // dfs(i-1,j)
         }
     }
 
